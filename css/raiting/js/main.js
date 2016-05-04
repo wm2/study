1bf72ae3504176fb061e86269aba6ca1bdaea43d
Vitaliy <games.wm2@gmail.com>
2016-05-04 15:50:59 +0300

    we add a js to index.html and we get a raiting value

--- css/raiting/js/main.js
+++ css/raiting/js/main.js
@@ -1,3 +1,12 @@
 /**
  * Created by user on 28.04.16.
  */
+$(document).ready(function () {
+    console.log("Ready");
+
+    $('a').click(function () {
+        console.log("Вы поставили рейтинг = " + $(this).text());
+        alert("Вы поставили рейтинг = " + $(this).text());
+    });
+
+});
