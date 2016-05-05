7c749de13b86adbff696910d87cb9bb403ef2319
Vitaliy <games.wm2@gmail.com>
2016-05-05 09:36:49 +0300

    add left block

--- css/raiting/js/main.js
+++ css/raiting/js/main.js
@@ -2,11 +2,28 @@
  * Created by user on 28.04.16.
  */
 $(document).ready(function () {
+var total;
+function show(){
     console.log("Ready");
 
-    $('a').click(function () {
-        console.log("Вы поставили рейтинг = " + $(this).text());
-        alert("Вы поставили рейтинг = " + $(this).text());
+    $('a').on("click", function () {
+        //if (total >= 0)
+        total = $(this).text();
+        if (total > 0){
+        console.log(total);
+            $('li').removeClass("current").removeAttr('style');
+            console.log("Класс и стили очищены");
+        }
+        var raiting = "Ваша оценка: ";
+        raiting += $(this).text();
+        console.log(raiting);
+        $(this).parent().addClass("current");
+        $(this).parent().css("width", "20" * $(this).text());
+        $('dt').text("Ваша оценка: "+ $(this).text() +" из 5 звезд");
     });
+
+}
+
+    show();
 
 });
